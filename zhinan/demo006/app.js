e327f7e76af6e03bafd30dfa699d556bfefc651a
Jarry <JarryDr@outlook.com>
2018-02-07 17:32:07 +0800

    2018年2月7日 17:32:05

---
+++ zhinan/demo006/app.js
@@ -0,0 +1,5 @@
+console.time('small loop');
+for (var i = 100000 - 1; i >= 0; i--) {
+	;
+}
+console.timeEnd('small loop');
---
+++ zhinan/demo007/trace.js
@@ -0,0 +1,10 @@
+var user = new Object();
+user.name = "lihongjiao";
+user.getName = function(){
+	return this.name;
+};
+user.setName = function(name){
+	this.name = name;
+};
+
+console.trace('trace');
